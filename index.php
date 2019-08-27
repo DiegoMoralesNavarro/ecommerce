b28f155557f5f7bac6ad7c50b82aa722e8c7a55a
unknown <diegomn07@gmail.com>
2019-08-27 19:38:18 -0300

    rotas

--- index.php
+++ index.php
@@ -1,24 +1,34 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
+
+
 
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
+
 $app->get('/', function() {
-    
-	
-$sql = new Hcode\DB\Sql();
 
-$result = $sql->select("SELECT * FROM tb_users");
-
-
-echo json_encode($result);
-
+//////// rotas ///////
+//contruct header
+$page = new Page();
+//metodo index
+$page->setTpl("index");
+//destrct footer
 
 });
 
 $app->run();
 
+
+
+
+
+
+
  ?>
+
